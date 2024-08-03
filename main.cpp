c2358969f5f791580caa0956b9d2dd179a2c088f
ZeroWolf233 <3174788113@qq.com>
2024-08-03 09:16:55 +0800

    练习局部变量

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
 
-float a;
-float b;
-float c;
-
 int main()
 {
-	a = 0.1;
-	b = 0.2;
+	int a;
+	int b;
+	int c;
+
+	a = 1;
+	b = 2;
 	c = a + b;
+
 	cout << c;
+
 	return 0;
 }
