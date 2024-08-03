0c8fb8dd2baed7894a40dec4340109c2c5c66fa5
ZeroWolf233 <3174788113@qq.com>
2024-08-03 09:18:44 +0800

    局部变量定义

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int a = 114, b = 514, c = 1919;
+
 int main()
 {
-	int a;
-	int b;
-	int c;
+	int a = 1,b = 2,c;
 
-	a = 1;
-	b = 2;
 	c = a + b;
 
 	cout << c;
